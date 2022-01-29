d5523500d39038b0ff4f22c6adad1ba5abfd2c34
cberto <bertolanicami@gmail.com>
2022-01-29 19:07:10 -0300

    card

---
+++ FBCard/Models/CreditCard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FBCard.Models
+{
+    public class CreditCard
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Headline { get; set; }
+        [Required]
+        public string NumberCard { get; set; }
+        [Required]
+        public string ExpirationDate { get; set; }
+        [Required]
+        public string CVV { get; set; }
+
+    }
+}
